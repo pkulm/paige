4b8dae4df83868e3f747a75d3dba437bfb7089a6
Paige Kulm <paige.kulm@eagles.ewu.edu>
2013-08-21 22:03:15 -0700

    getting the base

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+		
+	jQuery('#camera_wrap_1').camera({
+		thumbnails: false
+	});
+	
+	
+	/*
+var $window = $(window);
+        function checkWidth() {
+        if ($window.width() < 767) {
+            $('#title-column').removeClass('header40').addClass('header70');
+            $('#nav-column').removeClass('header60').addClass('header30');
+            };
+
+        if ($window.width() >= 768) {
+            $('#title-column').removeClass('header70').addClass('header40');
+            $('#nav-column').removeClass('header30').addClass('header60');
+        }
+    }
+    checkWidth();
+    $(window).resize(checkWidth);
+*/
+});
